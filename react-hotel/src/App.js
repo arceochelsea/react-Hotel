a24b36d0f3e14c4a8be9175cc8426114a3ebb321
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-10-06 18:00:47 -0400

    Skeleton for project

---
+++ react-hotel/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './Components/NavBar';
+
+function App() {
+  return (
+    <div>
+    <h1>yerrr</h1>
+    <NavBar />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ react-hotel/src/Components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div>
+        <button></button>  
+        <button></button>
+        </div>
+    )
+}
